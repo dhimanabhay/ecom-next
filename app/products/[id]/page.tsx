04da80b0d8a7ffc089050f3defaebdc1404be4ea
Abhay Dhiman <abhaydhiman@Abhays-MacBook-Air.local>
2025-04-20 17:27:41 -0700

    promise issue

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { stripe } from "@/lib/stripe";
 import { ProductDetail } from "@/components/product-detail";
 
-export default async function ProductPage(props: { params: { id: string } }) {
-  const { id } = await props.params;
+export default async function ProductPage(props: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
